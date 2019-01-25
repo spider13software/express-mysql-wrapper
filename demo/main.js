451b7d558b1d4a4f7685fbad8ef5c8252a4d687f
nikitamaksycmhyk@gmail.com <nikitamaksycmhyk@gmail.com>
2019-01-25 21:10:18 +0200

    First fix

--- demo/main.js
+++ demo/main.js
@@ -1,26 +1,23 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const mysql = require("mysql");
-const expressMysqlWrapper = require("../express-mysql-wrapper.js");
-const config = require("./config.json");
+const express = require('express');
+const mysql = require('mysql');
+const expressMysqlWrapper = require('../express-mysql-wrapper.js');
+const config = require('./config.json');
 
 const dbPool = mysql.createPool(config.mysql);
 
 var httpServer = express();
 httpServer.use(expressMysqlWrapper(dbPool, {
-	"connPropName": "dbConn",
-	"errorMethod": (error, request, response, next) => {
-		response.end("mysql error: " + error.message);
-	}
+	'connPropName': 'dbConn'
 }));
 
-httpServer.get("/", (request, response, next) => {
-	request.dbConn.query("SELECT 1 + 1 AS `value`", (error, rowList, fieldList) => {
+httpServer.get('/', (request, response, next) => {
+	request.dbConn.query('SELECT 1 + 1 AS `value`', (error, rowList, fieldList) => {
 		if(error) {
-			response.end("mysql error");
+			response.end('mysql error');
 		} else {
-			response.end("value is " + rowList[0].value);
+			response.end('value is ' + rowList[0].value);
 		}
 	})
 });
--- express-mysql-wrapper.js
+++ express-mysql-wrapper.js
@@ -1,29 +1,25 @@
-"use strict";
+'use strict';
+
+const onFinished = require('on-finished');
 
 module.exports = (mysqlPool, options) => {
-	var errorMethod = (error, request, response, next) => {
-		response.end("mysql error");
-	};
-	var connPropName = "mysqlConn";
-	if(options) {
-		if(options.errorMethod && typeof(options.errorMethod) == "function") {
-			errorMethod = options.errorMethod;
-		}
-		if(options.connPropName && typeof(options.connPropName) == "string") {
-			connPropName = options.connPropName
-		}
-	}
-	return (request, response, next) => {
-		mysqlPool.getConnection((error, mysqlConn) => {
-			if(error) {
-				errorMethod(error, request, response, next);
-			} else {
-				request[connPropName] = mysqlConn;
-				response.on("finish", () => {
-					mysqlConn.release();
-				});
-				next();
-			}
-		});
-	};
+  var connPropName = 'mysqlConn';
+  if (options) {
+    if (options.connPropName && typeof(options.connPropName) == 'string') {
+      connPropName = options.connPropName;
+    }
+  }
+  return (request, response, next) => {
+    mysqlPool.getConnection((error, mysqlConn) => {
+      if (error) {
+        next(error);
+      } else {
+        request[connPropName] = mysqlConn;
+        onFinished(response, function (error) {
+          mysqlConn.release();
+        });
+        next();
+      }
+    });
+  };
 };
